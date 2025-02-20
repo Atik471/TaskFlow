a2cc8abc0980932a7c2efd1fd3fc0461f43247da
Atik471 <108218772+Atik471@users.noreply.github.com>
2025-02-20 14:04:19 +0600

    added initial project setup

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <p className="TaskFlow">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
